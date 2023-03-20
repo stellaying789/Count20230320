7c624024b8c09e2d469547e02836e28fa57abe3d
stella <o1103009@gm.pu.edu.tw>
2023-03-20 11:17:32 +0800

    加入樂透功能

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.txv)
         txv.text = "0"
 
+
+    }
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
 
     }
 }
